9ef9df42c2bdc6a884544971fa4c517f47bcc3e8
nagaoyosuke <hw17a124@oecu.jp>
2019-02-19 15:05:53 +0900

    汎用的なスクリプト入れた

---
+++ Assets/Script/Manager/MySceneManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : SingletonMonoBehaviour<MySceneManager> {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void GoTitle(){
+		VSave.isVersus = false;
+		ESave.isEndless = false;
+		SceneManager.LoadScene("Title");
+	}
+
+	public static void GoLobby(){
+		SceneManager.LoadScene("Lobby");
+	}
+
+	public static void GoSilentcave(){
+		SceneManager.LoadScene("Silent Cave");
+	}
+
+	public static void GoTutorial(){
+		SceneManager.LoadScene("Tutorial");
+	}
+
+	public static void GoResult(){
+		SceneManager.LoadScene("Result");
+	}
+
+	public static void GoVersusReady(){
+		SceneManager.LoadScene("VersusReady");
+	}
+
+	public static void GoVersus(){
+		SceneManager.LoadScene("Versus");
+	}
+
+	public static void GoVersusTime(){
+		SceneManager.LoadScene("VersusTime");
+	}
+
+	public static void GoVersusScore(){
+		SceneManager.LoadScene("VersusScore");
+	}
+
+	public static void GoVersusResult(){
+		SceneManager.LoadScene("VersusResult");
+	}
+
+	public static void GoVersusResult2(){
+		SceneManager.LoadScene("VersusResult2");
+	}
+
+	public static void GoEndless(){
+		ESave.isEndless = true;
+		ESave.isResult = false;
+		SceneManager.LoadScene("Endless");
+	}
+
+	public static void GoEndless2(){
+		ESave.isEndless = true;
+		ESave.isResult = false;
+		SceneManager.LoadScene("Endless2");
+	}
+
+	public static void GoEndlessReady(){
+		SceneManager.LoadScene("EndlessReady");
+	}
+
+	public static void GoEndlessResult(){
+		ESave.isResult = true;
+		SceneManager.LoadScene("EndlessResult");
+	}
+
+	public static void GoEndlessNakane(){
+		ESave.isResult = true;
+		SceneManager.LoadScene("Nakane");
+	}
+
+	public static void GoEndlessOver(){
+		SceneManager.LoadScene("EndlessOver");
+	}
+
+	public static void GoEndlessStore(){
+		SceneManager.LoadScene("EndlessStore");
+	}
+
+	public static void GoModeSelect(){
+		SceneManager.LoadScene("ModeSelect");
+	}
+
+	public static void GoCredit(){
+		SceneManager.LoadScene("Credit");
+	}
+
+	public static void GoExplanatoryText(){
+		SceneManager.LoadScene("ExplanatoryText");
+	}
+
+}
---
+++ Assets/Script/Manager/SoundLoader.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//sound.csにse,bgmを読み込ませる
+//seの場合はsound.loadse,bgmはsound.loadbgm
+//se再生させるときはplayse("hogehogehoege")
+//https://qiita.com/2dgames_jp/items/20360f9797c7e8b166bc
+
+//読み込まれない時はファイル名を日本語から英語にしたらなおるときがある。
+
+public class SoundLoader : MonoBehaviour {
+
+	void Awake () {
+		Sound.LoadSe("mageSound01", "mageSound01");
+		Sound.LoadSe("mageSound02", "mageSound02");
+
+		Sound.LoadSe ("castSpell", "castSpell");
+		Sound.LoadSe ("pickLance", "回収");
+		Sound.LoadSe ("throwLance", "throwLance");
+		Sound.LoadSe ("counter", "カウンター成功時");
+
+		Sound.LoadSe ("parry01", "parry01");
+		Sound.LoadSe ("parry02", "parry02");
+
+		Sound.LoadSe ("loseLife", "loseLife");
+		Sound.LoadSe ("death", "死亡");
+		Sound.LoadSe ("end","試合終了");
+
+		Sound.LoadSe ("doorclose","扉（閉）");
+
+		Sound.LoadSe("damage01","damage01");
+		Sound.LoadSe("damage02","damage02");
+		Sound.LoadSe("damage03","damage03");
+		Sound.LoadSe("damage04","damage04");//ぽろん系ダメージ
+		Sound.LoadSe("damage05","何かにつかえるかも 2");//ぽろん系ダメージ
+
+		Sound.LoadSe ("spinningSlash01","回転斬り");
+		Sound.LoadSe ("spinningSlash02","回転斬り１");
+		Sound.LoadSe ("spinningSlash03","回転斬り２");
+		Sound.LoadSe ("spinningSlash04","回転斬り３");
+		Sound.LoadSe ("spinningSlash05","回転斬り４");
+
+		Sound.LoadSe ("chargeGauge01","gagemax1");
+		Sound.LoadSe ("chargeGauge02","gagemax2");
+		Sound.LoadSe ("chargeGauge03","gagemax3");
+		Sound.LoadSe ("chargeGauge04","gagemax4");
+
+		Sound.LoadSe("kabe","壁ぶち当たり");
+
+		Sound.LoadSe("choice","選択音");
+		Sound.LoadSe("kettei","決定音");
+
+		Sound.LoadSe("meteo","メテオ");
+		Sound.LoadSe("meteobakuha","メテオ爆破");
+		Sound.LoadSe ("hokou", "歩行音（一応）");
+
+		//コインゲットするときに使用0826
+		//http://taira-komori.jpn.org/game01.html
+		Sound.LoadSe ("coin01", "coin02");
+		Sound.LoadSe ("coin02", "coin04");
+		//https://on-jin.com/sound/sei.php?bunr=割れる&kate=食器
+		Sound.LoadSe ("ware", "ware");
+		//使ってないフリー素材
+		Sound.LoadSe ("WarriorLast", "WarriorLast");
+
+
+		//宝箱のアイテムに使用中
+		Sound.LoadSe("getitem","powerup10");
+		//http://taira-komori.jpn.org/game01.html パワーアップ10
+
+		Sound.LoadBgm ("Endless", "BGMGATI");
+		Sound.LoadBgm ("Title", "かなしみの城2初期画面BG(予定)");
+		Sound.LoadBgm ("Versus", "SKBGM");
+		//0811 フリー素材追加by豊田
+		//https://www.youtube.com/watch?v=FfxffuFWq6I
+		Sound.LoadBgm ("GameOver", "GameOver");
+		//エンディング、スタッフロール用BGM。
+		//https://wingless-seraph.net/material-music_8bit.html
+		//「プロローグ」のメロディを使用しているためセットでの使用をオススメします。
+		//なんかいいのあったら変えてくれby豊田
+		Sound.LoadBgm("CharacterSelect","BGMCHARA");
+
+	}
+}
