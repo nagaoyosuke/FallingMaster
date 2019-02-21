788768e489112e64f3fe01437e9f44808e5e4b41
nagaoyosuke <hw17a124@oecu.jp>
2019-02-21 11:54:07 +0900

    最初のカメラ演出一応できた

--- Assets/Script/MainScene/CameraMove.cs
+++ Assets/Script/MainScene/CameraMove.cs
@@ -5,13 +5,34 @@ using DG.Tweening;
 
 public class CameraMove : MonoBehaviour {
 
+    private Transform trans;
+
 	// Use this for initialization
 	void Start () {
-		
+        trans = transform;
+        StartCameraMove();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    void StartCameraMove(){
+        Sequence seq = DOTween.Sequence();
+        var MovePoint = new Vector3(5,trans.position.y,trans.position.z);
+        seq.Append(
+            transform.DOMove(MovePoint,1.0f)
+        );
+
+        MovePoint = new Vector3(1.28f,5.32f,-3.08f);
+        seq.Append(
+            trans.DOMove(MovePoint,1.0f)
+        );
+
+        seq.Join(
+               trans.DORotate(new Vector3(60,-20,0),1.0f)
+        );
+    }
+
 }
