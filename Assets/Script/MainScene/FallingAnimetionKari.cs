9210797ad5ddc8e50da22164f06bed6d9242289c
nagaoyosuke <hw17a124@oecu.jp>
2019-03-30 15:14:33 +0900

    スローモーションの動きだけ完成

---
+++ Assets/Script/MainScene/FallingAnimetionKari.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingAnimetionKari : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.rotation.x < -0.5f)
+        {
+            transform.rotation = new Quaternion(90.0f, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+            rb.freezeRotation = true;
+        }
+        print(transform.localRotation.x);
+
+    }
+}
--- Assets/Script/MainScene/MainSceneManager.cs
+++ Assets/Script/MainScene/MainSceneManager.cs
@@ -3,52 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using System;
+using DG.Tweening;
 /// <summary>
-/// ゲームの進行を管理するクラス(03/23)
+/// ゲームの進行を管理するクラス(03/23長尾)
 /// </summary>
 public class MainSceneManager : MonoBehaviour
 {
 
     /// <summary>
-    /// ここにゲームで起こるイベントを順番にインスペクターから登録
+    /// ここにゲームで起こる自動イベントをdotween記述で順番にインスペクターから登録
+    /// 入力待ちとかはできへんからムービーイベントやと思って
+    /// CameraMove.csのStartMove参考(03/24長尾)
     /// </summary>
-    public Func<Action<bool>,IEnumerator> Event;
+    public TweenEvent StarttweenEvent;
+    
+    /// <summary>
+    /// ゲーム全体の流れをまとめるシーケンス
+    /// </summary>
+    [SerializeField]
+    private Sequence Startsequence;
 
-    private bool isEnd;
-
-    // Start is called before the first frame update
-    IEnumerator Start(){
-        Event = test;
-        Event += test1;
-        StartCoroutine(Event(EndCheck));
-        StartCoroutine(Event(EndCheck));
-        yield return null;
+    void Awake() {
+        Startsequence = DOTween.Sequence();
     }
 
-    // Update is called once per frame
-    void Update(){
-        print(isEnd);
-    }
-
-    public void EndCheck(bool isEnd){
-        this.isEnd = isEnd;
-    }
-
-    public void testa(){
-        StartCoroutine(test(EndCheck));
-    }
-
-    public IEnumerator test(Action<bool> isEnd){
-        yield return new WaitForSeconds(1.0f);
-        print("sdfghj");
-        isEnd(true);
-
-    }
-
-    public IEnumerator test1(Action<bool> isEnd){
-        yield return new WaitForSeconds(1.0f);
-        print("e");
-        isEnd(true);
-
+    void Start(){
+        StarttweenEvent.Invoke(Startsequence);
     }
 }
+
+[Serializable]
+public class TweenEvent : UnityEvent<Sequence> { }
