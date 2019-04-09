20e3d33f3bb2209dc50d8467e89b968bbe50423a
o <yuyukosamadaisuki@gmail.com>
2019-04-09 21:57:53 +0900

    バグ修正

--- Assets/Script/TitleScene/GetTap.cs
+++ Assets/Script/TitleScene/GetTap.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 画面タップを管理するクラス(02/02豊田)
+/// </summary>
+
 public class GetTap : MonoBehaviour
 {
+    private bool isTapped = false;
+
 
     // Use this for initialization
     void Start()
@@ -14,9 +20,10 @@ public class GetTap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !isTapped)
         {
             Sound.PlaySe("osu01");
+            isTapped = true;
             StartCoroutine(WaitingFadeOut());
 
         }
