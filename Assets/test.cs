95929d3f01478a409589c1cd89e61f9e3dbdddce
o <hw17a114@oecu.jp>
2019-06-16 02:24:35 +0900

    メニューscene変更(仮)

---
+++ Assets/test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void gomain(){
+        panel.GetComponent<ScreenFader>().isFadeOut = true;
+        StartCoroutine(wait());
+    }
+
+
+    IEnumerator wait(){
+        yield return new WaitForSeconds(1.5f);
+        MySceneManager.GoMain();
+
+
+    }
+}
