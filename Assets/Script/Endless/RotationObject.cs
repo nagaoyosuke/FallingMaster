b1b9d326dfa5f737a22e743d8ea164926be5089d
nagaoyosuke <hw17a124@oecu.jp>
2020-01-05 05:25:36 +0900

    オブジェクトが回転するように

---
+++ Assets/Script/Endless/RotationObject.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObject : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Trs;
+
+    /// <summary>
+    /// 一周するまでにかけるフレーム
+    /// </summary>
+    [SerializeField]
+    private int flame;
+
+    public enum XYZ
+    {
+        X,
+        Y,
+        Z
+    }
+
+    [SerializeField]
+    private XYZ xyz;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Trs == null)
+            return;
+
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        float v = 360.0f / flame;
+        var baseVec = GetVector(v);
+        var vec = baseVec;
+
+        while (true)
+        {
+            for (int i = 0; i < flame; i++)
+            {
+                Trs.localEulerAngles += vec;
+                yield return new WaitForFixedUpdate();
+            }
+
+            Trs.localEulerAngles += GetVector(-360.0f);
+            vec = baseVec;
+        }
+    }
+
+    Vector3 GetVector(float v)
+    {
+        Vector3 value = Vector3.zero;
+
+        switch (xyz)
+        {
+            case XYZ.X:
+                value.x = v;
+                break;
+            case XYZ.Y:
+                value.y = v;
+                break;
+            case XYZ.Z:
+                value.z = v;
+                break;
+        }
+
+        return value;
+    }
+}
